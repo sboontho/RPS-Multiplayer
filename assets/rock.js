82b984a74ec97cdc65e7bc5a5453b116a55a2bca
sboontho <sboontho@hotmail.com>
2019-01-08 16:06:22 -0500

    First commit

---
+++ assets/rock.js
@@ -0,0 +1,76 @@
+
+
+function playWith(option){
+   // first, we deselect the previous selected element (if exist)
+   if (userOption != undefined)
+   {
+      userSelectionElement = document.getElementById(userOption);
+      // we remove the purple border (if exist)
+      userSelectionElement.style.border = "5px #F8F8F8  solid";
+   }	 		
+   // then, we select the user option and we add 
+   // a purple border
+   var userOption = option;
+   userSelectionElement = document.getElementById(option);
+   userSelectionElement.style.border = "5px #990066 solid";
+}
+
+function playGame(){
+    var computerOption = Math.random();
+ if (computerOption <0.34){
+     computerOption = "stone";
+ }else if(computerOption <=0.67){
+     computerOption = "paper";
+ }else{
+     computerOption = "scissor";
+ }
+ 
+ resultMessage = compare(userOption, computerOption);
+ 
+ document.getElementById("result").innerHTML = 
+ "<p>User select: " + userOption + " - Computer select:" 
+ + computerOption + "</p> <p>" + resultMessage + "</p>";
+ 
+}
+
+function compare(userSelection, computerSelection)
+		{
+			if(userSelection == undefined)
+			{
+				return "Please, select an option before play"
+			}	
+
+		    if (userSelection == computerSelection)
+		    {
+		       return "It is a draw!";
+		    }
+		    
+		    if (userSelection == "stone")
+		    {
+		        if (computerSelection == "scissor")
+		        {
+		             return "You win.";
+		        } else {
+		            return "The computer win. Try again.";
+		        }
+		        
+		    } else if (userSelection == "paper") {
+		        
+		        if (computerSelection == "stone")
+		        {
+		             return "You win." ;
+		        } else if("scissor") {
+		            return "The computer win. Try again." ;
+		        }
+		        
+		    } else if (userSelection == "scissor") {
+		      
+		        if (computerSelection == "stone")
+		        {
+		            return "The computer win. Try again.";
+		        }else{
+		            return "You win.";
+		        }
+		    }
+        }  
+        
